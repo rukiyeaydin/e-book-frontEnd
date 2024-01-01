880b6770ead869442fe94c1cfcbff279e89b9b21
Rukiye Aydın <rukiyeaydinlive@gmail.com>
2024-01-01 14:15:33 +0300

    modal page designed

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -19,20 +19,45 @@ export default function Modal() {
   }
 
   return (
+    // <>
+    //   <div onClick={toggleModal} className="btn-modal"><FaSearch className="modalikon"/></div>
+
+    //   {modal && (
+    //     <div className="modal">
+    //       <div onClick={toggleModal} className="overlaymodal"></div>
+    //       <div className="modal-content">
+    //         <input type="text" placeholder="Kitap Ara" className="modalinput"/>
+    //         <div className="modal-results">
+    //           <Link to="/kitap" className="modal-result" onClick={toggleModal}>Babalar ve Oğullar</Link>
+    //           <Link to="/kitap" className="modal-result" onClick={toggleModal}>Babalar ve Oğullar</Link>
+    //           <Link to="/kitap" className="modal-result" onClick={toggleModal}>Babalar ve Oğullar</Link>
+    //           <Link to="/kitap" className="modal-result" onClick={toggleModal}>Babalar ve Oğullar</Link>
+    //           <Link to="/kitap" className="modal-result" onClick={toggleModal}>Babalar ve Oğullar</Link>
+    //         </div>
+    //         <div className="close-modal" onClick={toggleModal}><IoIosClose className="close-modal-icon"/></div>
+    //       </div>
+    //     </div>
+    //   )}
+    // </>
     <>
       <div onClick={toggleModal} className="btn-modal"><FaSearch className="modalikon"/></div>
-
       {modal && (
         <div className="modal">
           <div onClick={toggleModal} className="overlaymodal"></div>
           <div className="modal-content">
-            <input type="text" placeholder="Kitap Ara" className="modalinput"/>
+            <div className="modalinput">
+              <input type="text" placeholder="Kitap Adı" className="modalinputin"/>
+            </div>
             <div className="modal-results">
-              <Link to="/kitap" className="modal-result" onClick={toggleModal}>Babalar ve Oğullar</Link>
-              <Link to="/kitap" className="modal-result" onClick={toggleModal}>Babalar ve Oğullar</Link>
-              <Link to="/kitap" className="modal-result" onClick={toggleModal}>Babalar ve Oğullar</Link>
-              <Link to="/kitap" className="modal-result" onClick={toggleModal}>Babalar ve Oğullar</Link>
-              <Link to="/kitap" className="modal-result" onClick={toggleModal}>Babalar ve Oğullar</Link>
+              <div className="modal-result">
+                <Link className="modal-result-link">Babalar ve Oğullar</Link>
+              </div>
+              <div className="modal-result">
+                <Link className="modal-result-link">Babalar ve Oğullar</Link>
+              </div>
+              <div className="modal-result">
+                <Link className="modal-result-link">Babalar ve Oğullar</Link>
+              </div>
             </div>
             <div className="close-modal" onClick={toggleModal}><IoIosClose className="close-modal-icon"/></div>
           </div>
